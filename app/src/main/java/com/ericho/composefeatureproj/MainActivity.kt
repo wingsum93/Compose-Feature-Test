746f6e3f87d2d0edd383e8a668174032b0a666e7
Kit Mak <kit.ck.mak@gtomato.com>
2021-12-15 18:20:17 +0800

    Add Instant build setting

--- app/src/main/java/com/ericho/composefeatureproj/MainActivity.kt
+++ app/src/main/java/com/ericho/composefeatureproj/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.ericho.composefeatureproj
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
@@ -10,8 +14,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.ericho.composefeatureproj.ui.TicketDisplayView
 import com.ericho.composefeatureproj.ui.theme.ComposeFeatureProjTheme
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
+import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener
 
 class MainActivity : ComponentActivity() {
+
+    private val instantModule by lazy { getString(R.string.title_restaurant_queue) }
+
+    private val listener = SplitInstallStateUpdatedListener { state ->
+        val multiInstall = state.moduleNames().size > 1
+        val langsInstall = state.languages().isNotEmpty()
+        val names = if (langsInstall) {
+            // We always request the installation of a single language in this sample
+            state.languages().first()
+        } else state.moduleNames().joinToString(" - ")
+
+        when (state.status()) {
+            SplitInstallSessionStatus.DOWNLOADING -> {
+
+            }
+            SplitInstallSessionStatus.REQUIRES_USER_CONFIRMATION -> {
+                manager.startConfirmationDialogForResult(state, this, CONFIRMATION_REQUEST_CODE)
+            }
+            SplitInstallSessionStatus.INSTALLED -> {
+                onSuccessfulLoad(names, launch = !multiInstall)
+            }
+            SplitInstallSessionStatus.INSTALLING -> {
+
+            }
+            SplitInstallSessionStatus.FAILED -> {
+                toastAndLog(getString(R.string.error_for_module, state.errorCode(),
+                    state.moduleNames()))
+            }
+        }
+    }
+    private lateinit var manager: SplitInstallManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,8 +62,44 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        manager = SplitInstallManagerFactory.create(this)
     }
+
+    override fun onResume() {
+        // Listener can be registered even without directly triggering a download.
+        manager.registerListener(listener)
+        super.onResume()
+    }
+
+    override fun onPause() {
+        // Make sure to dispose of the listener once it's no longer needed.
+        manager.unregisterListener(listener)
+        super.onPause()
+    }
+
+    private fun onSuccessfulLoad(moduleName: String, launch: Boolean) {
+        if (launch) {
+            when (moduleName) {
+                instantModule -> launchActivity(INSTANT_SAMPLE_CLASSNAME)
+            }
+        }
+    }
+
+    private fun launchActivity(className: String) {
+        val intent = Intent().setClassName(BuildConfig.APPLICATION_ID, className)
+        startActivity(intent)
+    }
+
 }
+
+fun MainActivity.toastAndLog(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    Log.d(TAG, text)
+}
+private const val TAG = "DynamicFeatures"
+private const val INSTANT_PACKAGE_NAME = "com.ericho.restaurant_queue"
+private const val INSTANT_SAMPLE_CLASSNAME = "$INSTANT_PACKAGE_NAME.PullActivity"
+private const val CONFIRMATION_REQUEST_CODE = 1
 
 @Composable
 fun Greeting(name: String) {
